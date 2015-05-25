3d400e27cc321aa2e9e31d1d56fc049e626948ee
amanjpro <amanj.sherwany@usi.ch>
2015-05-25 13:52:00 +0200

    Make Namers a proper compilation phase

--- tiny/src/main/scala/names/Namers.scala
+++ tiny/src/main/scala/names/Namers.scala
@@ -3,20 +3,36 @@ package ch.usi.inf.l3.sana.tiny.names
 import ch.usi.inf.l3.sana.tiny
 import tiny.source.Position
 import tiny.contexts.TreeContexts
+import tiny.util.CompilationUnits
 import tiny.contexts.TreeId
 import tiny.ast.Trees
+import tiny.types.Types
+import tiny.report._
+import tiny.passes
 import Names._
-import tiny.types._
 
  
-import scalaz.{Name => _, _}
+import scalaz.{Name => _, Failure => _, _}
 import Scalaz._
 
-trait Namers {
-  self: Trees with TreeContexts =>
+trait Namers extends passes.Phases {
+  self: Trees with TreeContexts with Types with CompilationUnits =>
 
-  trait Namer {
+  trait Namer extends TransformerPhase {
+    val name: String = "namer"
+    override val description: Option[String] = 
+      Some("The main type-checking phase.")
+    override def runRightAfter: Option[String] = Some("parser")
 
+
+    def startPhase(unit: CompilationUnit): 
+         (Vector[Failure], CompilationUnit) = {
+      val tree  = unit.tree
+      val state = unit.state
+      // val (w, typedTree, s) = run(typeTree(tree), state, Nil)
+      // (w, CompilationUnit(typedTree, s, unit.fileName))
+      ???
+    }
     def canRedefine: Boolean
 
     // def bind(id: TreeId, tree: IdentifiedTree): TreeState[IdentifiedTree] =
