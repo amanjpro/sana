f0f5b2d7497640b130e621bb2b90f3471a9d996e
amanjpro <amanj.sherwany@usi.ch>
2015-06-02 14:18:10 +0200

    Document Flag value class

--- tiny/src/main/scala/modifiers/flags.scala
+++ tiny/src/main/scala/modifiers/flags.scala
@@ -1,15 +1,38 @@
 package ch.usi.inf.l3.sana.tiny.modifiers
 
+/**
+  * This is a Value class to represent a flag.
+  *
+  * Using this class protects us from arbitrary integer
+  * values to be assigned as a flag.
+  *
+  * @constructor creates a new Flag with the given mask value
+  * @param mask the value of the flag
+  */
 class Flag(val mask: Int) extends AnyVal {
+  /** @see [[scala.Int.&(x:Int):*]] */
   def &(flag: Flag): Flag = Flag(flag.mask & mask)
+  /** @see [[scala.Int.|(x:Int):*]] */
   def |(flag: Flag): Flag = Flag(flag.mask | mask)
+  /** @see [[scala.Int.>>>(x:Int):*]] */
   def >>>(flag: Flag): Flag = Flag(flag.mask >>> mask)
+  /** @see [[scala.Int.>>(x:Int):*]] */
   def >>(flag: Flag): Flag = Flag(flag.mask >> mask)
+  /** @see [[scala.Int.<<(x:Int):*]] */
   def <<(flag: Flag): Flag = Flag(flag.mask << mask)
+  /** @see [[scala.Int.^(x:Int):*]] */
   def ^(flag: Flag): Flag = Flag(flag.mask ^ mask)
 }
-
+/**
+  * Companion object of class Flag
+  */
 object Flag {
+  /** 
+    * A factory method for [[Flag]]
+    *
+    * @param mask the value of the flag
+    * @return a new instance of Flag with the given mask value
+    */
   def apply(mask: Int): Flag = new Flag(mask)
 }
 
@@ -29,5 +52,3 @@ private class FlagsImpl extends Flags {
 trait FlagSet {
   val NO_FLAGS: Flag = Flag(0)
 }
-
-
