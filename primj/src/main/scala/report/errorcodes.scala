dbc91e76fa63caf14cf2b3a3b0a393755aac1c7d
amanjpro <amanj.sherwany@usi.ch>
2015-06-17 11:33:28 +0200

    Add codes for final variables and assignment

--- primj/src/main/scala/report/errorcodes.scala
+++ primj/src/main/scala/report/errorcodes.scala
@@ -11,7 +11,19 @@ case object VOID_VARIABLE_TYPE extends ErrorCode {
   val message: String = "Void type variable"
 }
 
+case object UNINITIALIZED_FINAL_VARIABLE extends ErrorCode {
+  val message: String = "Final variable is uninitialized"
+}
 
 case object TYPE_NOT_FOUND extends ErrorCode {
   val message: String = "Type not found"
+}
+
+
+case object REASSIGNING_FINAL_VARIABLE extends ErrorCode {
+  val message: String = "Reassigining to a final variable"
+}
+
+case object ASSIGNING_NOT_TO_VARIABLE extends ErrorCode {
+  val message: String = "Assigning to a tree that is not a variable"
 }
