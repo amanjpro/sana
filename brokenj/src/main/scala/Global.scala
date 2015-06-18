ab8e60305c0223d6dbfc58e6545c4527912a375f
amanjpro <amanj.sherwany@usi.ch>
2015-06-18 10:09:58 +0200

    Add utilities for brokenj AST nodes

---
+++ brokenj/src/main/scala/Global.scala
@@ -0,0 +1,20 @@
+package ch.usi.inf.l3.sana.brokenj
+
+import ch.usi.inf.l3.sana
+import sana.primj
+// import types.Types
+import ast.{Trees, TreeUtils}
+// import contexts.{TreeContexts, TreeInfos}
+// import util.Definitions
+
+
+
+
+trait Global extends primj.Global with 
+        Trees with
+        TreeUtils {
+        // TreeContexts with
+        // Definitions with
+        // TreeInfos with
+        // Types {
+}
---
+++ brokenj/src/main/scala/ast/TreeUtils.scala
@@ -0,0 +1,34 @@
+package ch.usi.inf.l3.sana.brokenj.ast
+
+import ch.usi.inf.l3.sana
+import sana.calcj
+import sana.primj
+import primj.ast
+
+
+trait TreeUtils extends ast.TreeUtils {
+  self: Trees =>
+
+    def canHaveLabel(tree: Tree): Boolean = tree match {
+      // INFO: Synchronize, Throw and Try to be added
+      case _: Label | _: If | _: While | _: For | _: Block |
+           _: Empty | _: Switch | _: Continue | _: Break   |
+           _: Return                                         =>
+        true  
+      case e                                                 =>
+        isValidStatementExpression(e)
+    }
+    def isContinuable(tree: Tree): Boolean = isIterTree(tree)
+
+    def isBreakable(tree: Tree): Boolean = tree match {
+      case _: Switch | _: While | _: For    => true
+      case _                                => false
+    }
+
+    def isIterTree(tree: Tree): Boolean = tree match {
+      case _: While | _: For            => true
+      case _                            => false
+    }
+  
+}
+
