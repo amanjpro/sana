ffb53335b8185a9aa8733f2cd57fa521cfaf2de7
amanjpro <amanj.sherwany@usi.ch>
2015-06-25 14:18:42 +0200

    Add OOJ's traits to Global

--- ooj/src/main/scala/Global.scala
+++ ooj/src/main/scala/Global.scala
@@ -2,10 +2,14 @@ package ch.usi.inf.l3.sana.ooj
 
 import ch.usi.inf.l3.sana
 import sana.brokenj
+
 import types.Types
 import ast.{Trees, Constants}
 // import contexts.{TreeContexts, TreeInfos}
+
 import util.Definitions
+import io.ClassFileParsers
+import contexts.{TreeInfos, TreeContexts}
 
 
 
@@ -14,6 +18,9 @@ trait Global extends brokenj.Global with
         Trees with
         Constants with
         Types with
+        ClassFileParsers with
+        TreeInfos with
+        TreeContexts with
         Definitions {
         // TreeContexts with
         // Definitions with
