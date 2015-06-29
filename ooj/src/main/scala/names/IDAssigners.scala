39cd6f628e14d2884d7564e806ae540d09a4ffee
amanjpro <amanj.sherwany@usi.ch>
2015-06-29 20:42:20 +0200

    Add the first version of IDAssigner for OOJ

---
+++ ooj/src/main/scala/names/IDAssigners.scala
@@ -0,0 +1,127 @@
+package ch.usi.inf.l3.sana.ooj.names
+
+import ch.usi.inf.l3.sana
+import sana.tiny
+import sana.primj
+import sana.brokenj
+import sana.ooj
+
+import tiny.source.Position
+import tiny.util.{CompilationUnits,MonadUtils}
+import tiny.contexts.{TreeId, NoId}
+import tiny.passes
+import tiny.report.Report
+import tiny.names.Name
+import tiny.debug.logger
+import tiny.io.ClassPathCatalog
+
+
+import primj.report._
+import primj.modifiers.Ops.noflags
+
+import ooj.Global
+ 
+import scalaz.{Name => _, Failure => _, _}
+import Scalaz._
+
+
+
+trait IDAssigners extends brokenj.names.IDAssigners {
+  override type G <: Global
+  import global._
+
+
+  trait IDAssigner extends super.IDAssigner {
+
+    import rwst.{local => _, _}
+
+    override def startPhase(state: Context, unit: CompilationUnit): 
+         (Vector[Report], CompilationUnit, Context) = unit.tree match {
+      case pkg: PackageDef => 
+        // the owner of a package name is the parent package!!
+        val (pid, cid, state2) = {
+          val pkgs = pkg.name.asString.split(".").toList
+          extendPackages(state, NoId, pkgs)
+        }
+        // The owner of a package is the parent package, and this can
+        // be found by going one step up from its id (path).
+        val pkg2 = PackageDef(pkg.id, pkg.name, pkg.members, pkg.pos, pid.up)
+
+        // The owner of the members of a package, is the compilation unit,
+        // which in turn is owned by the package
+        val (w, namedTree, s) = assignPackageDef(pkg).run(cid, state)
+        logger.debug(namedTree.show(s))
+        (w, CompilationUnit(cid, namedTree, unit.fileName), s)
+      case _               =>
+        super.startPhase(state, unit)
+    }
+
+    override def assign(tree: Tree): IDAssignerMonad[Tree] = tree match {
+      case pkg: PackageDef                           => for {
+        r       <- assignPackageDef(pkg)
+      } yield r
+      case clazz: ClassDef                           => for {
+        r       <- assignClassDef(clazz)
+      } yield r
+      case _                                         =>
+        super.assign(tree)
+    }
+
+
+    /**
+     * Assign id's the members of PackageDef. The package definition itself
+     * should have an id already by the caller.
+     */
+    def assignPackageDef(pkg: PackageDef): IDAssignerMonad[PackageDef] = for {
+      owner        <- ask
+      members      <- pkg.members.map((x) => 
+          local((_: TreeId) => owner)(assignDef(x))).sequenceU
+    } yield {
+      PackageDef(pkg.id, pkg.name, members, pkg.pos, pkg.owner)
+    }
+
+    /**
+     * Extends the context with the given nested packages `pkgs`.
+     * And returns the `id` for the inner most package, and
+     * its compilation unit id, and the resulting context.
+     */
+    protected def extendPackages(ctx: Context, prev: TreeId, 
+        pkgs: List[String]): (TreeId, TreeId, Context) = pkgs match {
+      case Nil              => (prev, NoId, ctx)
+      case (x::xs)          => 
+        val nme = Name(x)
+        val id = ctx.getContext(prev) match {
+          case None                =>
+            NoId
+          case Some(ctx)           =>
+            ctx.findInThisContext(nme, _.kind == PackageKind)
+        }
+        val (id2, ctx2) = if(id != NoId) {
+          (id, ctx)
+        } else {
+          val info        = newPackageDefInfo(nme)
+          val pkgContext  = packageContext(info)
+          ctx.extend(prev, pkgContext)
+        }
+
+        if(xs == Nil) {
+          // TODO: make it compilation unit context???
+          val (id3, ctx3) = ctx2.extend(id2, emptyContext)
+          (id2, id3, ctx3)
+        } else {
+          extendPackages(ctx2, id2, xs)
+        }
+    }
+
+
+    def assignClassDef(clazz: ClassDef): IDAssignerMonad[ClassDef] = for {
+      owner   <- ask
+      ctx1    <- get
+      id_ctx2 =  ctx1.extend(owner, classContext(clazz))
+      id      =  id_ctx2._1
+      ctx2    =  id_ctx2._2
+      body    <- assignTemplate(clazz.body)
+    } yield ClassDef(clazz.mods, id, clazz.name, clazz.parents, body, 
+                     clazz.pos, owner)
+  }
+}
