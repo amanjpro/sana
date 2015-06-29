311ffe85131ffaf8af3a64e5489af93d6a87ec4f
amanjpro <amanj.sherwany@usi.ch>
2015-06-29 20:41:56 +0200

    Add a preliminary version of TreeUtils for OOJ

---
+++ ooj/src/main/scala/ast/TreeUtils.scala
@@ -0,0 +1,21 @@
+package ch.usi.inf.l3.sana.ooj.ast
+
+import ch.usi.inf.l3.sana
+import sana.brokenj
+import brokenj.ast
+
+
+trait TreeUtils extends ast.TreeUtils {
+  self: Trees =>
+
+  override def pointsToUse(tree: Tree, 
+            p: UseTree => Boolean): Boolean = tree match {
+    case id: Ident         => p(id)
+    case tuse: TypeUse     => p(tuse)
+    case slct: Select      => p(slct)
+    case _                 =>
+      false
+  }
+  
+}
+
