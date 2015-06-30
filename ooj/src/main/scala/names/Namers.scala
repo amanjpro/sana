027641c6ff7800a70254a13f5389bdfc1d2613cf
amanjpro <amanj.sherwany@usi.ch>
2015-06-30 09:37:03 +0200

    Make Namer take care only about Naming top levels

--- ooj/src/main/scala/names/Namers.scala
+++ ooj/src/main/scala/names/Namers.scala
@@ -29,6 +29,7 @@ import Scalaz._
 trait Namers extends primj.names.Namers {
   self: IDAssigners =>
 
+  // Namer only resolves type-uses and never idents
   override type G <: Global
   import global._
 
@@ -39,6 +40,7 @@ trait Namers extends primj.names.Namers {
   trait Namer extends super.Namer {
 
 
+    // Call this when needed and fix it
     def loadFromClassPath(name: String, 
             isClass: Boolean, owner: TreeId): NamerMonad[Tree] = {
       // INFO: Tree needs to have ID's assigned to
@@ -82,28 +84,45 @@ trait Namers extends primj.names.Namers {
     
     override def nameUseTrees(use: UseTree): NamerMonad[UseTree] = use match {
       case tuse: TypeUse                             => for {
-        r    <- nameTypeUses(tuse)
+        r       <- nameTypeUses(tuse)
       } yield r
       case select: Select                            => for {
-        qual    <- nameUseTrees(select.qual)
+        qual    <- nameTrees(select.qual)
         ctx     <- getSW
         name    =  use.nameAtParser.map(Name(_)).getOrElse(ERROR_NAME)
         // qual is package name? then:
         // - if this package defines this name, then resolve this use 
-        // to that name, and it is a type use
+        //   to that name, and it is a type use
         // - if it doesn't, then resolve this to a package name.
-        // qual is type name or expression name? then this use is resolved
-        // to an expression name
-        res     <- ctx.getContext(qual.uses) match {
-          case None                                                    => 
-            // We know it has been wrongly resolved to a package name,
-            // but for now just return a Select with NoId
-            pointSW(Select(NoId, qual, select.pos, select.owner))
-          case Some(ctx: NamedContext) if ctx.tree.kind == PackageKind =>
-            val id = ctx.lookup(name, _.isInstanceOf[TypeKind], qual.uses)
-            pointSW(Select(id, qual, select.pos, select.owner))
-          case Some(ctx)                                               =>
-            val id = ctx.lookup(name, _.isInstanceOf[TermKind], qual.uses)
+        
+
+        // The following snippet only happens when we introduce inner classes
+        // qtpe    <- toNamerMonad(qual.tpe)
+        // id      =  qtpe match {
+        //   // This should not happen yet, until we introduce inner classes
+        //   case ct: ClassType => ct.id
+        //   // INFO: What about arrays? They do not have anything to do with
+        //   // type uses
+        //   case _             => NoId
+        // }
+        id      =  qual match {
+          case qual: UseTree       => qual.uses
+          case _                   => NoId
+        }
+        // Do we have a Type with this name seeing from the qual?
+        res     <- ctx.lookup(name, _.kind.isInstanceOf[TypeKind], id) match {
+          case NoId                                                    => 
+            ctx.lookup(name, _.kind == PackageKind, id) match {
+              case NoId             =>
+                // We know it has been wrongly resolved to a package name,
+                // but for now just return a Select with NoId
+                pointSW(Select(NoId, qual, select.pos, select.owner))
+              case id               =>
+                // This name points to a package
+                pointSW(Select(id, qual, select.pos, select.owner))
+            }
+          case id                                                      =>
+            // This name points to a type
             pointSW(Select(id, qual, select.pos, select.owner))
         }
       } yield res
