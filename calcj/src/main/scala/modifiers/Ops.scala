38b7b68a7b50d12a50d9a0a6d14a074a13ff9c51
amanjpro <amanj.sherwany@usi.ch>
2015-07-02 18:59:43 +0200

    Stripe trailing whitespaces

--- calcj/src/main/scala/modifiers/Ops.scala
+++ calcj/src/main/scala/modifiers/Ops.scala
@@ -7,8 +7,8 @@ import ch.usi.inf.l3.sana.tiny
 object Ops {
 
   val noflags = tiny.modifiers.Ops.noflags
-  
-  implicit class FlagOps(mask: tiny.modifiers.Flags) 
+
+  implicit class FlagOps(mask: tiny.modifiers.Flags)
       extends tiny.modifiers.Ops.FlagOps(mask) {
     def isPostfix: Boolean = mask.hasFlag(POSTFIX)
   }
--- calcj/src/main/scala/parser/Parsers.scala
+++ calcj/src/main/scala/parser/Parsers.scala
@@ -30,7 +30,7 @@ trait Parsers extends parser.Parsers {
 
   type G <: Global
   import global._
-  
+
   def parse(source: SourceFile): CompilationUnit = {
     val tree = new CalcjVisitor(source.name).visit(source.content)
     CompilationUnit(NO_COMPILATION_UNIT_ID, tree, source.name)
@@ -56,16 +56,16 @@ trait Parsers extends parser.Parsers {
         case "!"     => Not
       }
       (e1, op) match {
-        case (e: Expr, op: POp) if isPostfix => 
+        case (e: Expr, op: POp) if isPostfix =>
           Unary(Flags(POSTFIX), op, e, toTypeState(notype), pos(ctx))
-        case (e: Expr, op: UOp) => 
+        case (e: Expr, op: UOp) =>
           Unary(noflags, op, e, toTypeState(notype), pos(ctx))
         case _                  =>
           // TODO: report an error
           throw new Exception("Expression is expected")
       }
     }
-    def createBinary[T <: ParserRuleContext](es: java.util.List[T], 
+    def createBinary[T <: ParserRuleContext](es: java.util.List[T],
       trm: String, ctx: ParserRuleContext): Expr = {
       val e1 = visitChildren(es.get(0))
       val op = trm match {
@@ -91,32 +91,32 @@ trait Parsers extends parser.Parsers {
       }
       val e2 = visitChildren(es.get(1))
       (e1, e2) match {
-        case (x: Expr, y: Expr) => 
+        case (x: Expr, y: Expr) =>
           Binary(x, op, y, toTypeState(notype), pos(ctx))
         case _                  =>
           // TODO: report an error
           throw new Exception("Expression is expected")
       }
     }
-    
-    override def visitUnaryNum(@NotNull ctx: CalcjParser.UnaryNumContext): Tree = { 
-      createUnaryOrPostfix(false, ctx.expression, ctx.op.getText, ctx) 
+
+    override def visitUnaryNum(@NotNull ctx: CalcjParser.UnaryNumContext): Tree = {
+      createUnaryOrPostfix(false, ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitUnaryElse(@NotNull ctx: CalcjParser.UnaryElseContext): Tree = {
-      createUnaryOrPostfix(false, ctx.expression, ctx.op.getText, ctx) 
+      createUnaryOrPostfix(false, ctx.expression, ctx.op.getText, ctx)
     }
 
-    override def visitPostfix(@NotNull ctx: CalcjParser.PostfixContext): Tree = { 
-      createUnaryOrPostfix(true, ctx.expression, ctx.op.getText, ctx) 
+    override def visitPostfix(@NotNull ctx: CalcjParser.PostfixContext): Tree = {
+      createUnaryOrPostfix(true, ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitPrimitiveType(
-      @NotNull ctx: CalcjParser.PrimitiveTypeContext): Tree = { 
+      @NotNull ctx: CalcjParser.PrimitiveTypeContext): Tree = {
       TypeUse(NoId, Some(ctx.getText), pos(ctx), NoId)
     }
 
-    override def visitCast(@NotNull ctx: CalcjParser.CastContext): Tree = { 
+    override def visitCast(@NotNull ctx: CalcjParser.CastContext): Tree = {
       val e = visitChildren(ctx.expression)
       val tpt = visitChildren(ctx.primitiveType)
       (tpt, e) match {
@@ -127,85 +127,85 @@ trait Parsers extends parser.Parsers {
           throw new Exception("(TypeUse) Expression is expected")
       }
     }
-    
-    
+
+
     // Binary visitors
 
     override def visitMul(@NotNull ctx: CalcjParser.MulContext): Tree = {
-      createBinary(ctx.expression, ctx.op.getText, ctx) 
+      createBinary(ctx.expression, ctx.op.getText, ctx)
     }
-       
+
     override def visitAdd(@NotNull ctx: CalcjParser.AddContext): Tree = {
-      createBinary(ctx.expression, ctx.op.getText, ctx) 
+      createBinary(ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitShifts(@NotNull ctx: CalcjParser.ShiftsContext): Tree = {
-      createBinary(ctx.expression, ctx.op.getText, ctx) 
+      createBinary(ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitRel(@NotNull ctx: CalcjParser.RelContext): Tree = {
-      createBinary(ctx.expression, ctx.op.getText, ctx) 
+      createBinary(ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitEqu(@NotNull ctx: CalcjParser.EquContext): Tree = {
-      createBinary(ctx.expression, ctx.op.getText, ctx) 
+      createBinary(ctx.expression, ctx.op.getText, ctx)
     }
 
     override def visitBAnd(@NotNull ctx: CalcjParser.BAndContext): Tree = {
-      createBinary(ctx.expression, "&", ctx) 
+      createBinary(ctx.expression, "&", ctx)
     }
 
     override def visitBXor(@NotNull ctx: CalcjParser.BXorContext): Tree = {
-      createBinary(ctx.expression, "^", ctx) 
+      createBinary(ctx.expression, "^", ctx)
     }
-    
+
     override def visitBOr(@NotNull ctx: CalcjParser.BOrContext): Tree = {
-      createBinary(ctx.expression, "|", ctx) 
+      createBinary(ctx.expression, "|", ctx)
     }
 
-    override def visitAnd(@NotNull ctx: CalcjParser.AndContext): Tree =  { 
-      createBinary(ctx.expression, "&&", ctx) 
+    override def visitAnd(@NotNull ctx: CalcjParser.AndContext): Tree =  {
+      createBinary(ctx.expression, "&&", ctx)
     }
 
-    override def visitOr(@NotNull ctx: CalcjParser.OrContext): Tree =  { 
-      createBinary(ctx.expression, "||", ctx) 
+    override def visitOr(@NotNull ctx: CalcjParser.OrContext): Tree =  {
+      createBinary(ctx.expression, "||", ctx)
     }
 
 
 
-    
+
     // Literal visitors
 
-    override def visitIntLit(@NotNull ctx: CalcjParser.IntLitContext): Tree = { 
+    override def visitIntLit(@NotNull ctx: CalcjParser.IntLitContext): Tree = {
       val txt = ctx.getText
       (txt.endsWith("l") || txt.endsWith("L")) match {
-        case true  => Lit(LongConstant(ctx.getText.toInt), pos(ctx)) 
-        case false => Lit(IntConstant(ctx.getText.toInt), pos(ctx)) 
+        case true  => Lit(LongConstant(ctx.getText.toInt), pos(ctx))
+        case false => Lit(IntConstant(ctx.getText.toInt), pos(ctx))
       }
     }
-    
-    override def visitFloatLit(@NotNull ctx: CalcjParser.FloatLitContext): Tree = { 
+
+    override def visitFloatLit(@NotNull ctx: CalcjParser.FloatLitContext): Tree = {
       val txt = ctx.getText
       (txt.endsWith("f") || txt.endsWith("F")) match {
-        case true  => Lit(FloatConstant(ctx.getText.toInt), pos(ctx)) 
-        case false => Lit(DoubleConstant(ctx.getText.toInt), pos(ctx)) 
+        case true  => Lit(FloatConstant(ctx.getText.toInt), pos(ctx))
+        case false => Lit(DoubleConstant(ctx.getText.toInt), pos(ctx))
       }
     }
-   
-    override def visitCharLit(@NotNull ctx: CalcjParser.CharLitContext): Tree = { 
-      Lit(CharConstant(ctx.getText.head), pos(ctx)) 
+
+    override def visitCharLit(@NotNull ctx: CalcjParser.CharLitContext): Tree = {
+      Lit(CharConstant(ctx.getText.head), pos(ctx))
     }
 
-    // override def visitStrLit(@NotNull ctx: CalcjParser.StrLitContext): Tree = { 
-    //   Lit(StringConstant(ctx.getText), pos(ctx)) 
+    // override def visitStrLit(@NotNull ctx: CalcjParser.StrLitContext): Tree = {
+    //   Lit(StringConstant(ctx.getText), pos(ctx))
     // }
 
-  
-    override def visitBoolLit(@NotNull ctx: CalcjParser.BoolLitContext): Tree = { 
-      Lit(BooleanConstant(ctx.getText.toBoolean), pos(ctx)) 
+
+    override def visitBoolLit(@NotNull ctx: CalcjParser.BoolLitContext): Tree = {
+      Lit(BooleanConstant(ctx.getText.toBoolean), pos(ctx))
     }
 
-    
+
   }
 }
 
