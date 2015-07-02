80e9180ffbf8960148e0e32ab69768e1427dff35
amanjpro <amanj.sherwany@usi.ch>
2015-07-02 11:09:43 +0200

    Add errorcodes for Super and This checks

--- ooj/src/main/scala/report/errorcodes.scala
+++ ooj/src/main/scala/report/errorcodes.scala
@@ -10,3 +10,16 @@ case object AMBIGUOUS_METHOD_INVOCATION extends ErrorCode {
 case object INSTANCE_METHOD_IN_STATIC_CONTEXT_INVOK extends ErrorCode {
   val message: String = "Invoking an instance method in a static context"
 }
+
+case object ACCESSING_THIS_IN_STATIC extends ErrorCode {
+  val message: String = "Accessing this in a static context"
+}
+
+
+case object ACCESSING_SUPER_IN_STATIC extends ErrorCode {
+  val message: String = "Accessing super in a static context"
+}
+
+case object ACCESSING_SUPER_IN_OBJECT extends ErrorCode {
+  val message: String = "Accessing super in a Object class"
+}
